278162f5c0e67df5a116b8b7de2d3f9d791b7375
Ian Scott <chesnekov@gmail.com>
2020-07-29 14:38:11 -0700

    adds constructor, index, and create routes to CuisinesController.cs
    Co-authored-by: James Henager <jameschenager@gmail.com>

---
+++ BestRestaurant/Controllers/CuisinesController.cs
@@ -0,0 +1,38 @@
+using BestRestaurants.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  public class CuisinesController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public CuisinesController (BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Cuisine> model = _db.Cuisines.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Cuisine cuisine)
+    {
+      _db.Cuisines.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+  }
+}
