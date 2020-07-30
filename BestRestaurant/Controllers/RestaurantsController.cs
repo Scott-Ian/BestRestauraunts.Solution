3a66926a914617e4daf252ebdefa344620e4ad18
Ian Scott <chesnekov@gmail.com>
2020-07-30 11:23:53 -0700

    adds constructor and private readonly database context to RestaurantsController
    Co-authored-by: James Henager <jameschenager@gmail.com>

---
+++ BestRestaurant/Controllers/RestaurantsController.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestRestaurants.Models;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    
+  }
+}
